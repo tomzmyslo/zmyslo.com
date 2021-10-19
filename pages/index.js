8ec3e3d6c5b6b6926e929598ed3dfdcc5127e530
Tom Zmyslo <tom@zmyslo.com>
2021-10-19 01:24:13 -0400

    Adds tailwindcss and some basic styling (#2)

--- pages/index.js
+++ pages/index.js
@@ -3,12 +3,17 @@ import logo from '../public/zmyslo-retro-logo.png'
 
 function HomePage() {
   return(
-    <>
-      <Image
-        src={logo}
-        alt="Zmyslo Logo"
-      />
-    </>
+    <div className="container mx-auto">
+      <div className="flex justify-center">
+        <div className="h-screen my-auto -mt-20 flex items-center">
+          <Image
+            className="filter invert"
+            src={logo}
+            alt="Zmyslo Logo"
+          />
+        </div>
+      </div>
+    </div>
   )
 }
 
