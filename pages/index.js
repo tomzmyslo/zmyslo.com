c83db40298d28e23518175579ff797e7cb41f282
Tom Zmyslo <tom@zmyslo.com>
2022-10-10 19:16:40 -0400

    Adds link for LinkedIn (#7)

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Head from 'next/head';
 import Image from 'next/image';
 import Link from 'next/link';
 import Github from '../components/Github';
+import LinkedIn from '../components/LinkedIn';
 import logo from '../public/zmyslo-retro-logo.png';
 
 function HomePage() {
@@ -15,17 +16,22 @@ function HomePage() {
               <meta name='viewport' content='initial-scale=1.0, width=device-width' />
             </Head>
             <Image className='filter invert pb-8' src={logo} alt='Zmyslo Logo' />
-            <div className='flex justify-center space-x-2'>
+            <div className='flex justify-center space-x-2 mt-4'>
               <p className='text-xl font-light pb-2'>Ruby</p>
               <p className='text-xl font-light pb-2'>/</p>
               <p className='text-xl font-light pb-2'>Swift</p>
               <p className='text-xl font-light pb-2'>/</p>
               <p className='text-xl font-light pb-2'>JavaScript</p>
             </div>
-            <div className='flex justify-center'>
+            <div className='flex justify-center items-center space-x-4 mt-4'>
               <Link href='https://github.com/tomzmyslo'>
                 <a target='_blank'>
                   <Github />
+                </a>
+              </Link>
+              <Link href='https://www.linkedin.com/in/tomzmyslo'>
+                <a target='_blank'>
+                  <LinkedIn />
                 </a>
               </Link>
             </div>
