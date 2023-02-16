5c63336c5d5aad243a8060ba7c5d051e2e78d380
Tom Zmyslo <tom@zmyslo.com>
2023-02-15 19:04:06 -0500

    Makes some minor tweaks to increase accessibility (#11)

--- app/layout.js
+++ app/layout.js
@@ -1,19 +1,25 @@
-import Header from 'components/Header';
-import Footer from 'components/Footer';
-import { Inter } from '@next/font/google';
-import './global.css';
+import Header from "components/Header";
+import Footer from "components/Footer";
+import { Inter } from "@next/font/google";
+import "./global.css";
 
-const inter = Inter({ subsets: ['latin'] });
+const inter = Inter({ subsets: ["latin"] });
 
 export default function RootLayout({ children }) {
   return (
-    <html lang='en'>
+    <html lang="en">
       <head>
-        <title>tom zmyslo</title>
-        <meta name='viewport' content='initial-scale=1.0, width=device-width, viewport-fit=cover' />
-        <meta name='theme-color' content='#4f46e5' />
-        <meta name='description' content='Personal website of Tom Zmyslo.' />
-        <meta name='keywords' content='software engineer, ruby, javascript, swift, code' />
+        <title>Tom Zmyslo</title>
+        <meta
+          name="viewport"
+          content="initial-scale=1.0, width=device-width, viewport-fit=cover"
+        />
+        <meta name="theme-color" content="#4f46e5" />
+        <meta name="description" content="Personal website of Tom Zmyslo." />
+        <meta
+          name="keywords"
+          content="software engineer, ruby, javascript, swift, code"
+        />
       </head>
       <body
         className={`${inter.className} antialiased flex flex-col justify-between items-center h-screen bg-slate-900`}
