bc54f52a8a37d3b30bb3e7b6f1a3514c38df7797
Tom Zmyslo <tom@zmyslo.com>
2023-02-15 19:13:27 -0500

    Fixes heading elements to be sequential

--- components/Project.js
+++ components/Project.js
@@ -7,10 +7,10 @@ export default function Project({ project }) {
 
   return (
     <div className="px-4 pb-2 mb-8 border-l-4 border-indigo-600">
-      <h3 className="font-bold text-3xl">{name}</h3>
+      <h2 className="font-bold text-3xl">{name}</h2>
       <p className="text-sm font-semibold mb-3">{medium}</p>
       <div className="py-25 mb-4" dangerouslySetInnerHTML={createMarkup()} />
-      <h4 className="font-semibold text-sm mb-1">Technologies:</h4>
+      <h3 className="font-semibold text-sm mb-1">Technologies:</h3>
       <ul className="list-disc pl-4 marker:text-teal-400">
         {technologies.map((technology, i) => {
           return (
