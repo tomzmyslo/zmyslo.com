61334b735f7e87b8c4ae8c549f725e2039070ed4
Tom Zmyslo <tom@zmyslo.com>
2023-02-19 20:15:33 -0500

    Updates resume and adds a PDF copy for download

--- components/Section.js
+++ components/Section.js
@@ -1,10 +1,10 @@
 export default function Section({ children, name }) {
   return (
-    <section className='text-indigo-100 py-2'>
+    <section className="text-indigo-100 py-2">
       {name && (
         <>
-          <h2 className='text-xl font-black uppercase'>{name}</h2>
-          <hr className='mb-2' />
+          <h2 className="text-xl font-black uppercase">{name}</h2>
+          <hr className="mb-2 border-x border-slate-500" />
         </>
       )}
       {children}
